757d23664c4b359ee203f6c35dde7aebe81cc1ee
thaiyal0109 <swethaiyal15@gmail.com>
2025-11-09 21:24:03 -0800

    Add files via upload

---
+++ src/TNS T030307457_THAIYALNAYAGI_UserModule/src/test/java/placement/UserServicePlacementApplicationTests.java
@@ -0,0 +1,13 @@
+package placement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServicePlacementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
