f834eeaadf6997228c835632d3df19ad14961b6e
thaiyal0109 <swethaiyal15@gmail.com>
2025-12-19 09:38:55 +0530

    Add files via upload

---
+++ sprint 1_UserModule/UserModule/TNS T030306803_BalajiR_UserModule/src/main/java/placement/UserRepository.java
@@ -0,0 +1,14 @@
+package placement;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	List<User> findAll();
+    // You can define custom query methods if needed
+}
---
+++ sprint 1_UserModule/UserModule/TNS T030306803_BalajiR_UserModule/src/main/java/placement/UserServicePlacementApplication.java
@@ -0,0 +1,13 @@
+package placement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServicePlacementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServicePlacementApplication.class, args);
+	}
+
+}
